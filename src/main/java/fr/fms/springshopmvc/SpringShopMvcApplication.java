6e9baa9921585ac2fcd8819ba2890d83da0bf6b0
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-03-31 13:36:34 +0200

    first commit

---
+++ src/main/java/fr/fms/springshopmvc/SpringShopMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.springshopmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/fms/springshopmvc/SpringShopMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.springshopmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
