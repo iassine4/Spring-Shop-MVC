710779668aa6b07646aee1895f0465d38fee3b4c
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-04-01 14:57:18 +0200

    add ArticleRepository

---
+++ src/main/java/fr/fms/springshopmvc/repository/ArticleRepository.java
@@ -0,0 +1,13 @@
+package fr.fms.springshopmvc.repository;
+
+import fr.fms.springshopmvc.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ * Repository Spring Data JPA pour l'entité Article.
+ * Spring créera automatiquement l'implémentation au démarrage.
+ */
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
